4bef13552f0510192f22cffee4f814460655cd54
DaniMaki <daniilmak@yahoo.ca>
2020-03-24 15:23:02 -0700

    Made the Add Food button on Main Activity work.

--- app/src/main/java/com/griffins/whatsinmyfridge/MainActivity.java
+++ app/src/main/java/com/griffins/whatsinmyfridge/MainActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button AddFood;
+    private ImageButton AddFood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
